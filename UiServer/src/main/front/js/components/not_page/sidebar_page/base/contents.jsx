c8355bd3f48d926216645e48cb634432545fd3f3
nosoosso <nosoosso.null@gmail.com>
2016-10-17 14:44:01 +0900

    デザインの変更に追従

--- UiServer/src/main/front/js/components/not_page/sidebar_page/base/contents.jsx
+++ UiServer/src/main/front/js/components/not_page/sidebar_page/base/contents.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 export class Contents extends React.Component {
     render() {
         return (
-                <div id="main">
-                    <div className="main_inner">
+                <div id="main"  className="fr h100p w100p">
+                    <div className="main_inner tal h100p">
                         {this.props.children}
                     </div>
                 </div>
