05ec21122bb7f60acf8e4c34c4e10a9b986b8375
れむ <tmrsadden1984@gmail.com>
2016-10-24 15:51:25 +0900

    jsx化

--- UiServer/src/main/front/js/components/routes/main_page.jsx
+++ UiServer/src/main/front/js/components/routes/main_page.jsx
@@ -6,7 +6,103 @@ export class MainPage extends React.Component {
     return (
       <div>
         <ProductAndMemberSidebar products={this.props.products} member={this.props.member}>
-          <div className="plz_select">hogehoge</div>
+          {/*<!--main content-->*/}
+                <div id="main" className="fr h100p w100p">
+                    <div className="main_inner tal h100p">
+                        {/*<!--<div className="plz_select">Please select Product.</div>-->*/}
+                        
+                        
+                        
+                        
+                        {/*<!--チケット一覧、プロダクトトークン(タブメニュー)要素ここから-->*/}
+                        
+                        
+                        
+                        
+                        {/*<!-- TAB CONTROLLERS -->*/}
+                        <div id="tab_menu">
+                        <input type="radio" name="nav" id="one" className="tab_input"/>
+                        <label htmlFor="one">PRODUCT TOKEN</label>
+
+                        <input type="radio" name="nav" id="two" className="tab_input"/>
+                        <label htmlFor="two">REPORT</label>
+
+                        <article className="content one">
+                            <h1>Token</h1>
+                        </article>
+                        <article className="content two">
+                            
+
+                            <div id="search">
+                                <table>
+                                  <tbody>
+                                    <tr>
+                                        <th>
+                                            <span>Search </span>
+                                        </th>
+                                        <th>
+                                            <input type="search" className="search_box" />
+                                        </th>
+                                        <th>
+                                            <a href="">
+                                               <span className="search_btn dib">
+                                                   <img src="assets/img/search.png" alt="検索" />
+                                               </span>
+                                            </a> 
+                                        </th>
+                                    </tr>
+                                  </tbody>
+                                </table>  
+                            </div>
+
+                            <div className="ticket_area m0a">
+
+                                {/*<!--チケットここから-->*/}
+                                <a href="">
+                                    <div className="ticket">
+                                        <div className="t_title">Hogehogehoge</div>
+                                        <div className="t_data">
+                                            <table>
+                                              <tbody>
+                                                <tr>
+                                                    <th>yuusendo</th>
+                                                    <th>tantou</th>
+                                                    <th>2016/07/12</th>
+                                                    <th>joutai</th>
+                                                    <th>version</th>
+                                                    <th>
+                                                        <div className="comment">
+                                                            <img className="fl" src="assets/img/comment2.png" alt="コメント数"  />
+                                                            <span>6</span>
+                                                        </div>
+                                                    </th>
+                                                </tr>
+                                              </tbody>  
+                                            </table>
+                                        </div>
+                                        <div className="t_border"></div>
+                                        </div>
+                                </a>
+                                {/*<!--ここまで-->*/}
+                                
+                                
+                            </div>
+                        </article>
+                        </div>
+                        
+                        
+                        
+                        
+                        
+                        {/*<!--チケット一覧、プロダクトトークン(タブメニュー)-->*/}
+                        
+                        
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
         </ProductAndMemberSidebar>
       </div>
     );
